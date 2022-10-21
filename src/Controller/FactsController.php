18797dc6c4b0f9ea956bda2da36c013b0f5c7e4f
chidodesigns <chidoukaigwe@gmail.com>
2022-10-21 05:50:14 +0100

    notes updated

--- src/Controller/FactsController.php
+++ src/Controller/FactsController.php
@@ -85,13 +85,11 @@ class FactsController extends AbstractController
 
 
 //  Notes Cannot have two object keys named expression - so bug/flaw in current DSL
-//  Abstract The CustomBadExceptions Out Of Model Layer and Back into Controller
-//  Checkn Expression Args does not check for 1 expression sent needs a refactor to check 
+//  Abstract The CustomBadExceptions Out Of Model Layer and Back into Controller 
 //  JSON Decode vs Symfony Serializer (had issues trying to get this installed)
 //  Using POSTMAN had issues duplicating two fields in JSON post BODY so have not built/test for two arguments of the same type.
 //  After building v1 I reaslise the current UML model of the DSL is to verbose - which I created - therefore the querybuilder is very rudimentary and only tackles a few use cases - it needs to be more dynamic.
 //  Better use for handling expressions would be using the Symfony Expression Language it would allow expressions and expression methods to be created a lot more smoothly.
-//  Would need to add a fixtures function loop through both data sets in security and attribute and create both entities with a value assigned next to them for the fact's table.
 
 //  Postman Req:
 
